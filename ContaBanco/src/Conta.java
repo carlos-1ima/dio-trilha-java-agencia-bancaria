f6c28b4802437dedfbc93ef130e8f5616994c13d
Carlos Lima <144251910+CarlosDev258@users.noreply.github.com>
2024-11-04 14:09:13 -0300

    feat: Implementação de atributos e métodos na classe Conta; implementação de código para interação com o usuário na classe ContaTerminal.

--- ContaBanco/src/Conta.java
+++ ContaBanco/src/Conta.java
@@ -1,3 +1,45 @@
 public class Conta {
 
+    int numero;
+    String agencia;
+    String cliente;
+    double saldo;
+
+    public int getConta() {
+        return this.numero;
+    }
+
+    public void setConta(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return " " + this.agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getCliente() {
+        return this.cliente;
+    }
+
+    public void setCliente(String nome) {
+        this.cliente = nome;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String toString() {
+        return "Olá " + getCliente() + ", obrigado por criar uma conta em nosso banco,"
+        + " sua agência é" + getAgencia() + ", conta " + getConta() + " e seu saldo " + getSaldo()
+        + " já está disponível para saque!";
+    }
 }
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-      
+        Scanner sc = new Scanner(System.in);
+        Conta conta = new Conta();
+
+        System.out.println("Informe o número da Agência: ");
+        conta.setAgencia(sc.nextLine());
+
+        System.out.println("Informe o número da Conta: ");
+        conta.setConta(sc.nextInt());
+
+        sc.nextLine();
+
+        System.out.println("Digite seu Nome: ");
+        conta.setCliente(sc.nextLine());
+
+        System.out.println("Digite o Saldo da conta: ");
+        conta.setSaldo(sc.nextDouble());
+    
+        sc.nextLine();
+        sc.close();
+
+        System.out.println(conta.toString());
     }
 }
